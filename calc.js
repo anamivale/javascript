e478b4b14b5f43cc68aee2af45172a423ef5e673
Valeria Muhembele <61202849+anamivale@users.noreply.github.com>
2022-06-21 21:02:11 +0300

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,21 +1,23 @@
 console.log(
   "you can do addition(+) Subtraction(-) Multiplication(*) Division(/)"
 )
-// let sign = prompt("Enter your sign") can oly work in a browser
+// let sign = prompt("Enter your sign") can only work in a browser
+//let a = prompt("Enter first number")
+//let b = prompt("Enter second number")
 let sign = "/"
 const calc = (a, b) => {
   if (sign == "+") {
     let result = a + b
-    console.log(result)
+    return(result)
   } else if (sign == "-") {
     let result = a - b
-    console.log(result)
+    return(result)
   } else if (sign == "*") {
     let result = a * b
-    console.log(result)
+    return(result)
   } else if (sign == "/") {
     let result = a / b
-    console.log(result)
+    return(result)
   }
 }
 calc(1, 2)
