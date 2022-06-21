ec0b8ea718b74aef7514a53d19881d13e74b03e7
Valeria Muhambele <muhembelev@gmail.com>
2022-06-21 19:57:10 +0300

    introduction file

---
+++ intro.js
@@ -0,0 +1,6 @@
+// Write a javascript code that prints out your name, height, and country on the screen
+
+let name = "valeria muhembele"
+let height = "155cm"
+let country = "Kenya"
+console.log(`My name is ${name} and i am ${height} tall.I am from kenta`)
