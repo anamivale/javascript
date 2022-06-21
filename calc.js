4baef720bf716510dd34b27e90befa088bc02e15
Valeria Muhambele <muhembelev@gmail.com>
2022-06-21 20:29:25 +0300

    calc

--- calc.js
+++ calc.js
@@ -1,1 +1,21 @@
-// The calculator should be able to perform basic operations like Addition, Subtraction, Multiplication, & Division
+console.log(
+  "you can do addition(+) Subtraction(-) Multiplication(*) Division(/)"
+)
+// let sign = prompt("Enter your sign") can oly work in a browser
+let sign = "/"
+const calc = (a, b) => {
+  if (sign == "+") {
+    let result = a + b
+    console.log(result)
+  } else if (sign == "-") {
+    let result = a - b
+    console.log(result)
+  } else if (sign == "*") {
+    let result = a * b
+    console.log(result)
+  } else if (sign == "/") {
+    let result = a / b
+    console.log(result)
+  }
+}
+calc(1, 2)
